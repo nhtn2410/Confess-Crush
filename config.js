8c047fa59057ce12460501b13a9c6cbf094be1ad
nhtn2410 <107113631+nhtn2410@users.noreply.github.com>
2022-06-08 17:46:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ const CONFIG = {
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: 'Liệu cậu có hiểu tấm lòng này to lớn biết nhường nào, cậu hãy cùng tớ viết nên một câu chuyện ngọt ngào đó nho',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
